46026bad5578244edc616f620361974abeea7a40
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-04-26 12:51:54 +0100

    add telescope plugin with find files and live grep keymaps

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+})
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
