2c62d21869e0e8e447d9e117039000bbd2f78a60
Otmane Boumediane <boumediane.otmane@gmail.com>
2026-04-26 13:15:49 +0100

    configure lua_ls for neovim development

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,3 +13,25 @@ require("mason-lspconfig").setup({
 require("mason-tool-installer").setup({
 	ensure_installed = { "prettierd" },
 })
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
